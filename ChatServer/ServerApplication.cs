6d50a25167f5787f0e59661ac0999d387626535a
ArthurPai <baidragoon@gmail.com>
2017-07-16 12:31:59 +0800

    setup server basic architecture

--- ChatServer/ServerApplication.cs
+++ ChatServer/ServerApplication.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Photon.SocketServer;
 
 namespace ChatServer
 {
-    public class ServerApplication
+    public class ServerApplication : ApplicationBase
     {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            // 建立與Client端的連線，並將建立好的Peer傳回給Photon Server
+            return new ServerPeer(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            // 初始化Game Server
+        }
+
+        protected override void TearDown()
+        {
+            // 關閉Game Server，並釋放資源
+        }
     }
 }
--- ChatServer/ServerPeer.cs
+++ ChatServer/ServerPeer.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
 
 namespace ChatServer
 {
-    public class ServerPeer
+    public class ServerPeer : ClientPeer
     {
+        public ServerPeer(InitRequest initRequest) : base(initRequest)
+        {
+            // 建構子
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            // 斷線處理，例如釋放資源
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            // 收到Client傳過來的要求，並且加以處理
+        }
     }
 }

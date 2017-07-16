bb66a58826b50569017a158e83b15a07debe37f3
ArthurPai <baidragoon@gmail.com>
2017-07-16 15:23:54 +0800

    implement login mechanism

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -4,24 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ExitGames.Client.Photon;
+using ChatProtocol;
 
 namespace ChatClient
 {
+    enum GameState
+    {
+        Initiation,
+        Connected,
+        OnLogin,
+        Chatting,
+        Disconnected,
+    }
+
     class Program : IPhotonPeerListener
     {
+        private GameState m_State;
+        private PhotonPeer m_Peer;
+        
+        public Program()
+        {
+            m_State = GameState.Initiation;
+            m_Peer = new PhotonPeer(this, ConnectionProtocol.Udp);
+        }
+
         static void Main(string[] args)
         {
-            var listener = new Program();
-            var peer = new PhotonPeer(listener, ConnectionProtocol.Udp);
+            new Program().Run();
+        }
 
-            if (peer.Connect("127.0.0.1:4530", "ChatServer"))
+        public void Run()
+        {
+            if (m_Peer.Connect("127.0.0.1:4530", "ChatServer"))
             {
                 do
                 {
-                    Console.WriteLine(".");
-
                     // 讓Photon的service可以處理網路資料，這樣OnOperationResponse及OnEvent才會被觸發
-                    peer.Service();
+                    m_Peer.Service();
+
+                    switch(m_State)
+                    {
+                        case GameState.Initiation:
+                            break;
+                        case GameState.Connected:
+                            LoginToServer();
+                            m_State = GameState.OnLogin;
+                            break;
+                        case GameState.OnLogin:
+                            break;
+                        case GameState.Chatting:
+                            break;
+                        case GameState.Disconnected:
+                            break;
+                    }
 
                     System.Threading.Thread.Sleep(500);
                 } while (true);
@@ -30,7 +65,7 @@ namespace ChatClient
 
         public void DebugReturn(DebugLevel level, string message)
         {
-            Console.WriteLine(string.Format("{0}: {1}", level, message));
+            Console.WriteLine(string.Format("[{0}] {1}", level, message));
         }
 
         public void OnEvent(EventData eventData)
@@ -46,17 +81,78 @@ namespace ChatClient
         public void OnOperationResponse(OperationResponse operationResponse)
         {
             // 取得Server對Request的回應
+            switch (operationResponse.OperationCode)
+            {
+                case (byte)OperationCode.Login:
+                    HandleLoggedIn(operationResponse);
+                    break;
+                default:
+                    DebugReturn(DebugLevel.WARNING, "Unknown Response: " + operationResponse.OperationCode);
+                    break;
+            }
         }
 
         public void OnStatusChanged(StatusCode statusCode)
         {
             // 連線狀態變化的通知
 
-            Console.WriteLine("Peer status update:" + statusCode.ToString());
+            DebugReturn(DebugLevel.INFO, "Peer status update:" + statusCode.ToString());
 
             switch (statusCode)
             {
                 case StatusCode.Connect:
+                    m_State = GameState.Connected;
+                    break;
+                case StatusCode.Disconnect:
+                    m_State = GameState.Disconnected;
+                    break;
+            }
+        }
+
+        private void LoginToServer()
+        {
+            Console.WriteLine("\n請輸入帳號：");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\n請輸入密碼：");
+            string password = Console.ReadLine();
+
+            var parameters = new Dictionary<byte, object>
+            {
+                { (byte)LoginParameterCode.Name, name },
+                { (byte)LoginParameterCode.Password, password },
+            };
+
+            m_Peer.OpCustom((byte)OperationCode.Login, parameters, true);
+        }
+
+        private void HandleLoggedIn(OperationResponse operationResponse)
+        {
+            switch (operationResponse.ReturnCode)
+            {
+                case (short)ErrorCode.Ok:
+                    {
+                        int ret = Convert.ToInt16(operationResponse.Parameters[(byte)LoginResponseCode.Ret]);
+                        int id = Convert.ToInt16(operationResponse.Parameters[(byte)LoginResponseCode.ID]);
+                        string token = Convert.ToString(operationResponse.Parameters[(byte)LoginResponseCode.Token]);
+                        string name = Convert.ToString(operationResponse.Parameters[(byte)LoginResponseCode.Name]);
+                        string nick_name = Convert.ToString(operationResponse.Parameters[(byte)LoginResponseCode.Nickname]);
+
+                        Console.WriteLine("Login succeed ({0}) \nUser ID: {1} \nToken: {2} \nName: {3} \nNickName: {4}", ret, id, token, name, nick_name);
+                        m_State = GameState.Chatting;
+                    }
+                    break;
+                case (short)ErrorCode.InvalidParameter: // 參數錯誤
+                    DebugReturn(DebugLevel.INFO, operationResponse.DebugMessage);
+                    m_State = GameState.Connected;
+                    break;
+                case (short)ErrorCode.InvalidAccountOrPassword: // 帳號密碼錯誤
+                    DebugReturn(DebugLevel.INFO, operationResponse.DebugMessage);
+                    m_State = GameState.Connected;
+                    break;
+                default:
+                    DebugReturn(DebugLevel.WARNING, "Unknown RetureCode: " + operationResponse.ReturnCode);
+                    m_State = GameState.Connected;
                     break;
             }
         }
--- ChatServer/ServerPeer.cs
+++ ChatServer/ServerPeer.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Photon.SocketServer;
 using PhotonHostRuntimeInterfaces;
+using ChatProtocol;
 
 namespace ChatServer
 {
     public class ServerPeer : ClientPeer
     {
+        private int m_UserID;
+        private string m_Token;
+        private string m_UserName;
+        private string m_NickName;
+        private static Random random = new Random();
+
         public ServerPeer(InitRequest initRequest) : base(initRequest)
         {
             // 建構子
@@ -23,6 +30,75 @@ namespace ChatServer
         protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
         {
             // 收到Client傳過來的要求，並且加以處理
+            switch(operationRequest.OperationCode)
+            {
+                case (byte)OperationCode.Login:
+                    HandleLogin(operationRequest, sendParameters);
+                    break;
+            }
+        }
+
+        private void HandleLogin(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            OperationResponse respone;
+
+            if (operationRequest.Parameters.Count < 2)
+            {
+                // 參數錯誤
+                respone = new OperationResponse((byte)OperationCode.Login)
+                {
+                    ReturnCode = (short)ErrorCode.InvalidParameter,
+                    DebugMessage = "Login Failed",
+                };
+
+                SendOperationResponse(respone, sendParameters);
+                return;
+            }
+
+            var name = (string)operationRequest.Parameters[(byte)LoginParameterCode.Name];
+            var password = (string)operationRequest.Parameters[(byte)LoginParameterCode.Password];
+
+            if (name != "test" || password != "111111")
+            {
+                // 帳號密碼錯誤
+                respone = new OperationResponse((byte)OperationCode.Login)
+                {
+                    ReturnCode = (short)ErrorCode.InvalidAccountOrPassword,
+                    DebugMessage = "Invalid Name or Password",
+                };
+
+                SendOperationResponse(respone, sendParameters);
+                return;
+            }
+
+            int Ret = 1;
+            m_UserID = 1;
+            m_Token = RandomString(16);
+            m_UserName = name;
+            m_NickName = "Tester";
+            var parameters = new Dictionary<byte, object>
+            {
+                { (byte)LoginResponseCode.Ret, Ret },
+                { (byte)LoginResponseCode.ID, m_UserID },
+                { (byte)LoginResponseCode.Token, m_Token },
+                { (byte)LoginResponseCode.Name, m_UserName },
+                { (byte)LoginResponseCode.Nickname, m_NickName },
+            };
+
+            respone = new OperationResponse((byte)OperationCode.Login, parameters)
+            {
+                ReturnCode = (short)ErrorCode.Ok,
+                DebugMessage = "Login Succeed",
+            };
+
+            SendOperationResponse(respone, sendParameters);
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
